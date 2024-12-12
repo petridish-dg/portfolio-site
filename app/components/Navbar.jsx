3bcd7da8d08dd63c680ecd8bc7630e0e4298d264
Peter de Guzman <mail@peterdg.com>
2024-12-12 15:03:21 -0600

    Added Navigation

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,12 @@
+export default function Navbar() {
+  return (
+    <header>
+      <nav>
+        <div className="flex justify-between">
+          <div>Logo</div>
+          <div>Menu</div>
+        </div>
+      </nav>
+    </header>
+  );
+}
